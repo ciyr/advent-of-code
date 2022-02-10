160ef7ba43319d26e18c193a3229652a07e8475d
C <chaitanyaiyer01@gmail.com>
2022-02-10 12:18:08 +0530

    day 10

---
+++ day10/solution.py
@@ -0,0 +1,61 @@
+from collections import deque
+import statistics
+with open("input.txt", "r") as f:
+    input_data = f.read()
+    input_data = input_data.split("\n")
+count = 0
+final = []
+for i in range(len(input_data)):
+    stack = []
+    for j in range(len(input_data[i])):
+        if input_data[i][j] == "(" or input_data[i][j] == "[" or input_data[i][j] == "{" or input_data[i][j] == "<":
+            stack.append(input_data[i][j])
+        else:
+            if input_data[i][j] == ")":
+                if stack[-1] == "(":
+                    stack.pop()
+                else:
+                    count += 3
+                    break
+            if input_data[i][j] == "]" :
+                if stack[-1] == "[":
+                    stack.pop()
+                else:
+                    count += 57
+                    break
+            if input_data[i][j] == "}":
+                if stack[-1] == "{":
+                    stack.pop()
+                else:
+                    count += 1197
+                    break
+            if input_data[i][j] == ">":
+                if stack[-1] == "<":
+                    stack.pop()
+                else:
+                    count += 25137
+                    break
+        if j == len(input_data[i]) - 1:
+            if len(stack) != 0:
+                final.append(stack)
+print(count)
+result = []
+for i in final:
+    #reverse i (closing bracket stack will be opposite of opening bracket stack)
+    i.reverse()
+    score = 0
+    for j in i:
+        if j == "(":
+            score = score*5
+            score += 1
+        elif j == "[":
+            score = score*5
+            score += 2
+        elif j == "{":
+            score = score*5
+            score += 3
+        else:
+            score = score*5
+            score += 4
+    result.append(score)
+print(statistics.median(result))
